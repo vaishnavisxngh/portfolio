d2dc3a181a0b19e44b779dbe6be90422da407097
vaishnavisxngh <vaishnavi.29.singh@gmail.com>
2025-07-11 17:01:32 +0530

    Removed icons from footer and updated FooterLink type

--- app/constants/footer.ts
+++ app/constants/footer.ts
@@ -4,31 +4,26 @@ export const FOOTER_LINKS: FooterLink[] = [
   {
     name: 'LinkedIn',
     hoverText: 'Connect with me',
-    icon: 'C:\Users\KIIT0001\Desktop\p\public\icons\linkedin.svg',
     url: 'https://www.linkedin.com/in/vaishnavi-singh-3923a328a/',
   },
   {
     name: 'GitHub',
     hoverText: 'Trying to keep up',
-    icon: 'C:\Users\KIIT0001\Desktop\p\public\icons\github.svg',
     url: 'https://github.com/vaishnavisxngh',
   },
   {
     name: 'Instagram',
     hoverText: '@vaishnavisxngh',
-    icon: 'C:\Users\KIIT0001\Desktop\p\public\icons\instagram.svg',
     url: 'https://www.instagram.com/vaishnavisxngh/',
   },
   {
     name: 'Resume',
     hoverText: 'Download',
-    icon: 'public/icons/file.svg',
     url: './Vaishnavi-Singh Resume.pdf',
   },
   {
     name: 'Email',
     hoverText: 'Contact me',
-    icon: 'public/icons/email.svg',
     url: 'mailto:vaishnavi.29.singh@gmail.com',
   }
 ];
